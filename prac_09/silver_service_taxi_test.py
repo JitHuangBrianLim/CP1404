840b1a1c194ce46a73b50c2a529d8edeea323f6a
Jit Huang Brian Lim <jithuangbrian.lim@my.jcu.edu.au>
2023-01-15 21:05:05 +0800

    Initial commit

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,10 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    taxi = SilverServiceTaxi("Hummer", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
